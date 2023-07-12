563e2164020b5642b4ea74d9d3e7a70fe2d08b83
blakebonasera <blakebonasera@gmail.com>
2023-07-12 14:07:44 -0700

    Finished with styling on each page and ready for my application

--- task_calendar/main/forms.py
+++ task_calendar/main/forms.py
@@ -4,4 +4,4 @@ from .models import Task
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields = ('day', 'title', 'description','urgency')
+        fields = ('day', 'title', 'description','urgency','progress')
--- task_calendar/main/models.py
+++ task_calendar/main/models.py
@@ -11,10 +11,26 @@ class Task(models.Model):
         ('Sunday', 'Sunday'),
     )
 
+    URGENCY_CHOICES = (
+        ("Low","Low"),
+        ("Mid","Mid"),
+        ("High","High"),
+        ("GET IT DONE", "GET IT DONE"),
+    )
+
+    PROGRESS_CHOICES = (
+        ("Just Starting","Just Starting"),
+        ("25%","25%"),
+        ("50%","50%"),
+        ("75%","75%"),
+        ("Finished", "Finished"),
+    )
+
     day = models.CharField(max_length=10, choices=DAY_CHOICES)
     title = models.CharField(max_length=100)
-    description = models.TextField()
-    urgency = models.IntegerField()
+    description = models.CharField(max_length=155)
+    urgency = models.CharField(max_length=20, choices=URGENCY_CHOICES)
+    progress = models.CharField(max_length=20, choices=PROGRESS_CHOICES)
 
     def __str__(self):
         return self.title
